475c431eb17b92b719c7579a345c1960b9163b6d
stephen-chinekeboy <idokostephen93@gmail.com>
2026-01-22 16:46:58 +0100

    worked on gallery and 480 media query

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-  function showSideBar() {
-      const sidebar = document.querySelector('.sidebar')
-      sidebar.style.display = 'flex'
-    }
+function showSideBar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+  }
 
-  function hideSideBar() {
-      const sidebar = document.querySelector('.sidebar')
-      sidebar.style.display = 'none'
-    }
+function hideSideBar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+  }
 
-  document.querySelector('.js-register').addEventListener('click' , () => {
-    window.location.href="/register.html"
-  });
+// document.querySelector('.js-register').addEventListener('click' , () => {
+//   window.location.href="/register.html"
+// });
 
-
-
+  
+// document.querySelector('button').addEventListener('click', () => {
+//   window.location.href="/register.html"
+// })
